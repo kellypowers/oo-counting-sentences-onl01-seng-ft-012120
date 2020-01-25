c1e826d9b814d92bf4f97f858033f3c8af6fa297
kellypowers <kpowers.nj@gmail.com>
2020-01-25 17:07:37 -0500

    passes tests

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
+
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array = []
+    sentence_array = self.split
+    sentence_array.each do |word|
+      if word.sentence? || word.question? || word.exclamation?
+        new_array<< word
+      end
+    end
+    #puts new_array
+    new_array.count
   end
 end
